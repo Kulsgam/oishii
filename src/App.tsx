70729d7334fbcfc669cd096297729f36cf4c765c
Kulsgam <kuls.gam@gmail.com>
2025-03-15 11:14:34 +1100

    chore: add default theming

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh dark:bg-yellow-400 bg-red-500">
       <Button>Click me</Button>
     </div>
   );
